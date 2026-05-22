2a3af37a3a956f3dcb4a3cc6d5a641b566c278a0
Las45 <matteo.morent@gmail.com>
2026-05-22 12:25:23 +0200

    finished the news

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 async function ladeNachrichten() {
-    const url = "https://www.tagesschau.de/api2/news/";
+    const url = "https://www.tagesschau.de/api2u/news/";
     const response = await fetch(url);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    const ausgabe = document.getElementById("ausgabe");
+
+    ausgabe.innerHTML = "";
+
+    data.news.forEach(nachrichten => {
+        ausgabe.innerHTML += `
+        <h2>${nachrichten.title}</h2>
+        <p>${nachrichten.firstSentence ?? ""}</p>
+        <a href="${nachrichten.shareURL}" target="_blank">Artikel öffnen</a>
+        <hr>
+        `;
+    })
 }
